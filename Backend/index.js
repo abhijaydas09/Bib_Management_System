5d0eaf096a6392b19f5420b49891af69c009a004
abhijaydas09 <abhijay982004@gmail.com>
2025-07-10 21:47:49 +0530

    check mongodb url in .env

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+import {connect} from "mongoose";
+import connectDB from "./model/DatabaseConnectionDriver.js";
 
-let port = 3000;
+dotenv.config()
+let port = process.env.PORT || 6000 ;
+
 
 let app = express();
 
@@ -8,6 +13,9 @@ app.get("/",(req,res)=>{
     res.send("hello");
 })
 
+
 app.listen(port , ()=>{
+    connectDB()
     console.log("server started");
 })
+
